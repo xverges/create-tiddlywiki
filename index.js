e6f148abfbd7e65db244eba7eb783955c356e5f3
Xavier Vergés <460418+xverges@users.noreply.github.com>
2021-01-27 10:26:28 +0100

    Remove direct install + write next steps

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+const { yellow } = require('kleur')
+
 const userInput = require('./lib/user-input')
 const twCli = require('./lib/twcli')
 const resources = require('./lib/resources')
@@ -7,4 +9,9 @@ const resources = require('./lib/resources')
 userInput().then((requirements) => {
   twCli.create(requirements.folder, requirements.edition)
   resources.install()
+  console.log(`
+    Ready!
+    You can now run ${yellow('npm install')} to complete the installation.
+    You can then start the server running ${yellow('npm run tiddlywiki -- ' + requirements.folder + ' --listen')}
+  `)
 })
