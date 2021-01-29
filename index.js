e31862825c36f7be636a1c2d284922cbbe8e7f2d
Xavier Vergés <460418+xverges@users.noreply.github.com>
2021-01-29 07:35:34 +0100

    Use JSON5. More informative output.

--- index.js
+++ index.js
@@ -11,9 +11,13 @@ const howToStart = (requirements) => {
   const indent = '    '
   if (requirements.runtime.includes('tiddlywiki')) {
     help.push(`${indent}* start the tiddlywiki server running ${yellow('npx tiddlywiki ' + requirements.tiddlywikis[0].folder + ' --listen')}`)
+    help.push(`${indent}  (or just ${yellow('npm start')} after updating package.json with scripts.start="tiddlywiki ${requirements.tiddlywikis[0].folder}  --listen")`)
+    help.push(`${indent}  More info at https://tiddlywiki.com/static/WebServer.html`)
   }
   if (requirements.runtime.includes('tiddlyserver')) {
     help.push(`${indent}* start tiddlyserver running ${yellow('npx tiddlyserver --config tiddlyserver.json')}`)
+    help.push(`${indent}  (or just ${yellow('npm start')} if you update package.json with scripts.start="tiddlyserver --config tiddlyserver.json")`)
+    help.push(`${indent}  More info at https://arlen22.github.io/tiddlyserver/docs/serverconfig.html`)
   }
   if (help.length) {
     help.unshift('You can then')
